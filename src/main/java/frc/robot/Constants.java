502c9b716d6ec45b0acff8f0e55d9b5f4bce3bb1
calebabr <86934442+calebabr@users.noreply.github.com>
2023-12-05 18:04:29 -0600

    Gear Ratio, Correct Translational Joystick

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -55,7 +55,7 @@ public class Constants {
     public static final double DRIVE_GEAR_RATIO = 6.75 / 1.0; // 6.75:1
     public static final double DRIVE_ROTATIONS_TO_METERS = WHEEL_CIRCUMFERENCE / DRIVE_GEAR_RATIO;
     public static final double DRIVE_RPM_TO_METERS_PER_SECOND = DRIVE_ROTATIONS_TO_METERS / 60.0;
-    public static final double ANGLE_GEAR_RATIO = 21.43 / 1.0; // 12.8:1
+    public static final double ANGLE_GEAR_RATIO = 21.43 / 1.0; // 21.43:1
     public static final double ANGLE_ROTATIONS_TO_RADIANS = (Math.PI * 2) / ANGLE_GEAR_RATIO;
     public static final double ANGLE_RPM_TO_RADIANS_PER_SECOND = DRIVE_ROTATIONS_TO_METERS / 60.0;
 
